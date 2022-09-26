6b5712fea3c02ed74f2a949730709099a4369d9c
Noahkimtai <noahkimtai24@gmail.com>
2022-09-26 16:08:34 +0300

    Completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
+
+
 
 function Item({ name, category }) {
+  const [className,setClassName] = useState('')
+  const [elementText, setElementText] = useState('Add to Cart')
+  
+  function handleAddToCart(e){
+    const newClassName = className==''? 'in-cart':'';
+    setClassName(newClassName);
+    const newElementText =elementText=='Add to Cart'?'Remove from Cart':'Add to Cart'
+    setElementText(newElementText)
+  }
+
   return (
-    <li className="">
+    <li  className={className}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={(e)=>handleAddToCart(e)} className='Add'>{elementText}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
-
+//When I use the data imported from Item I get .filter is not a function in react js
 function ShoppingList({ items }) {
+  const [filterBy,setFilterBy]=useState('All')
+
+  function handleFilterChange(e){
+    setFilterBy(e.target.value)
+  }
+  const hardCodedItems = [
+    { id: 1, name: "Yogurt", category: "Dairy" },
+    { id: 2, name: "Pomegranate", category: "Produce" },
+    { id: 3, name: "Lettuce", category: "Produce" },
+    { id: 4, name: "String Cheese", category: "Dairy" },
+    { id: 5, name: "Cookies", category: "Dessert" },
+  ]
+  const newItems = hardCodedItems.filter((item)=>{
+    if(filterBy=='All'){
+      return true;
+    }else{
+      return item.category==filterBy;
+    };
+  });
+  
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={(e)=>handleFilterChange(e)}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +33,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {newItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
